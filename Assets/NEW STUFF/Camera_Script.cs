3042c1e8a16f59b8a09aa4e62c8ee91d2d8a391f
DIY <diyproductions@hotmail.com>
2021-03-02 11:27:28 +0000

    Importing 3d models
    Importing 3d models

---
+++ Assets/NEW STUFF/Camera_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Script : MonoBehaviour
+{
+    GameObject cam;
+    Rigidbody rb;
+    void Start()
+    {
+        cam = this.gameObject;
+        rb = cam.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) rb.AddForce(Vector3.forward * 2, ForceMode.Impulse);
+        else if (Input.GetKey(KeyCode.S)) rb.AddForce(Vector3.forward * -2, ForceMode.Impulse);
+        else if (Input.GetKey(KeyCode.A)) rb.AddForce(Vector3.up * 2, ForceMode.Impulse);
+        else if (Input.GetKey(KeyCode.D)) rb.AddForce(Vector3.down * 2, ForceMode.Impulse);
+        else rb.velocity = new Vector3(0, 0, 0);
+    }
+}
---
+++ Assets/NEW STUFF/New Clock/Clock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+    float currentTime = 0f, startingTime = 180f;
+    public TextMeshProUGUI timer;
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        timer.text = currentTime.ToString("0") /*+ " sec"*/;
+
+        if (currentTime <= 0) currentTime = 0;
+    }
+}
